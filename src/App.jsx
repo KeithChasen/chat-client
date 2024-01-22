873c394c77f3d91e51555653b6bc92918954bd1d
keithchasen <keithchasen89@gmail.com>
2024-01-22 22:27:06 +0100

    bootstrap css added

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Chat from './pages/Chat';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <>
+    <Container className='text-secondary'>
       <Routes>
         <Route path="/" element={<Chat/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register/>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </>
+    </Container>
   )
 }
 
