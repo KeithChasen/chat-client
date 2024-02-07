b209e335bdd0b5e8a6ad9642d1fd37828c8a9b26
keithchasen <keithchasen89@gmail.com>
2024-02-07 15:17:28 +0100

    implemeted chat box

--- src/components/chat/ChatBox.jsx
+++ src/components/chat/ChatBox.jsx
@@ -2,6 +2,8 @@ import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { ChatContext } from "../../context/ChatContext";
 import { useFetchRecipient } from "../../hooks/useFetchRecipient";
+import { Stack } from 'react-bootstrap';
+import moment from 'moment';
 
 const ChatBox = () => {
     const { user } = useContext(AuthContext);
@@ -23,7 +25,36 @@ const ChatBox = () => {
         )
     }
 
-    return ( <>ChatBox</> );
+    if (messagesLoading) {
+        return (
+            <p style={{ 
+                textAlign: "center",
+                width: "100%" 
+            }}>Loading chat...</p>
+        )
+    }
+
+    return ( 
+        <Stack gap={4} className="chat-box">
+            <div className="chat-header">
+                <strong>{recipientUser.name}</strong>
+            </div>
+            <Stack gap={3} className="messages">
+                { messages && messages.map((message, index) => (
+                    <Stack 
+                        key={index} 
+                        className={`message flex-grow-0 ${message.senderId === user._id ? 
+                            'self align-self-end' : 
+                            'align-self-start'}`
+                        }
+                    >
+                        <span>{message.text}</span>
+                        <span className="message-footer">{moment(message.createdAt).calendar()}</span>
+                    </Stack>
+                ))}
+            </Stack>
+        </Stack> 
+    );
 }
  
 export default ChatBox;
