458d7db6e78365a8d654c10039cb06c4ee0ff70d
keithchasen <keithchasen89@gmail.com>
2024-02-07 15:31:48 +0100

    input field for messaging with emojis

--- src/components/chat/ChatBox.jsx
+++ src/components/chat/ChatBox.jsx
@@ -1,9 +1,10 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { ChatContext } from "../../context/ChatContext";
 import { useFetchRecipient } from "../../hooks/useFetchRecipient";
 import { Stack } from 'react-bootstrap';
 import moment from 'moment';
+import InputEmoji from 'react-input-emoji';
 
 const ChatBox = () => {
     const { user } = useContext(AuthContext);
@@ -15,6 +16,9 @@ const ChatBox = () => {
     } = useContext(ChatContext);
 
     const { recipientUser } = useFetchRecipient(currentChat, user);
+    const [textMessage, setTextMessage] = useState('');
+
+    console.log('text', textMessage)
 
     if (!recipientUser) {
         return (
@@ -52,6 +56,14 @@ const ChatBox = () => {
                         <span className="message-footer">{moment(message.createdAt).calendar()}</span>
                     </Stack>
                 ))}
+            </Stack>
+            <Stack direction="horizontal" gap={3} className="chat-input flex-grow-0">
+                <InputEmoji 
+                    value={textMessage} 
+                    onChange={setTextMessage}
+                    fontFamily='nunito'
+                    borderColor="rgba(72, 112, 223, 0.2)"
+                />
             </Stack>
         </Stack> 
     );
